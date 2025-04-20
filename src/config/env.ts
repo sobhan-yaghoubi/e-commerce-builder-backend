8905d415735c9535fbd42835b8ef43982a2ced61
sobhan yaghoubi <sobhan.yaghoubi.work@gmail.com>
2025-04-20 09:44:54 +0330

    update => validator storeConfig file And schema of prisma and env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,5 +4,6 @@ dotenv.config()
 
 export const env = {
   PORT: process.env.PORT || 3000,
+  DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "secret",
 }
---
+++ src/validator/storeConfig.validator.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+import { ThemeEnum } from "./enums.validator"
+
+export const BaseStoreConfigSchema = z.object({
+  theme: ThemeEnum,
+  domain: z.string().url().min(1, "domain is required"),
+  languages: z.array(z.string().min(2)),
+  default_language: z.string().min(2, "default_language is required"),
+})
+
+export const CreateStoreConfigSchema = BaseStoreConfigSchema.refine(
+  (data) => data.languages.includes(data.default_language),
+  {
+    message: "default_language must be one of the value of languages filed",
+    path: ["default_language"],
+  }
+)
+
+export const UpdateStoreConfigSchema = BaseStoreConfigSchema.partial()
+
+export type CreateStoreConfigInput = z.infer<typeof BaseStoreConfigSchema>
+
+export type UpdateStoreConfigInput = z.infer<typeof UpdateStoreConfigSchema>
